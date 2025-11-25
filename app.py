691ee8e71462ab3168ec18378fd4aa5832ef4315
ssdyed <yvesemmanuel2@gmail.com>
2025-11-24 20:34:37 -0500

    Ajout des interprétations 3

--- app.py
+++ app.py
@@ -119,8 +119,8 @@ if not data.empty and len(data) > 0 :
         st.warning(f"Tendance baissière : La simulation suggère une correction potentielle de {variation_prevue:.2f}%.")
 
     st.info(f"""
-    **En résumé :** Le prix actuel est de {prix_actuel:.2f}$.
-    Selon le scénario médian, le prix cible est de {prix_median:.2f}$.
+    **En résumé :** Le prix actuel est de {prix_actuel:.2f}$. 
+    Selon le scénario médian, le prix cible est de {prix_median:.2f}$. 
     Attention toutefois, dans un scénario de crise, le prix pourrait descendre jusqu'à {valeur_risque_95:.2f}$.
     """)
 
